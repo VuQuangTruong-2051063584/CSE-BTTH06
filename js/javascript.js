7ce0fcf4b066e8176856aed9fc11ad58907075dc
VuQuangTruong-2051063584 <2051063584@e.tlu.edu.vn>
2022-03-17 23:06:12 +0700

    Form Array

---
+++ js/javascript.js
@@ -0,0 +1,15 @@
+function tao_mang(){
+    var songaunhien = [];
+    var length = Math.floor(Math.random()*(10)+10)
+    for(i=0;i<=length;i++){
+        songaunhien.push(Math.floor(Math.random()*100))  
+   }
+   
+}
+
+
+   
+
+
+
+
